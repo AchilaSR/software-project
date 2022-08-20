7967bd107013579770833c0cedd1440c84f152ff
shimmifathi <shimmifathi1999@gmail.com>
2022-08-20 12:45:16 +0530

    changed font

--- frountend/src/App.js
+++ frountend/src/App.js
@@ -2,12 +2,13 @@ import {BrowserRouter as Router, Route,Switch} from "react-router-dom";
 import Firstpage from "./pages/Firstpage";
 import Signinpage from "./pages/Signinpage";
 import Registerpage from "./pages/Registerpage";
+import Commentpage from "./pages/Commentpage";
 
 function Landingpage() {
   return (
     <Router>
      <Switch>
-     <Route exact path="/" component={Firstpage}/>
+     <Route exact path="/" component={Commentpage}/>
         <Route exact path="/singinpage" component={Signinpage}/>
         <Route exact path="/registerpage" component={Registerpage}/>
        
