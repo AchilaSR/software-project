d6ba5a6f55d743582bd0b917887e8f064c707f6b
shimmifathi <shimmifathi1999@gmail.com>
2022-08-20 18:36:35 +0530

    comment page edit

--- frountend/src/pages/Commentpage.js
+++ frountend/src/pages/Commentpage.js
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
+
 import { Stack, Box, Spacer } from '@chakra-ui/react'
 import Userheder from '../components/Userheder'
 import Post from '../components/Post'
  
-export default function Commentpage() {
+ function Commentpage() {
    return (
       <Stack alignItems="center">
          <Userheder/>
@@ -20,39 +20,7 @@ export default function Commentpage() {
       </Stack>
    )
 }
-=======
-import { Stack,Text } from "@chakra-ui/react";
-import Header from "../components/Header";
 
 
-
-function Commentpage() {
-
-   return (
-
-<Stack>
-
-<Text>
-   kjngfgmxdf
-</Text>
-
-
-
-
-</Stack>
-
-
-
-
-
-   )
-
-
-
-
-
-}
     
    export default Commentpage
-
->>>>>>> 79b9d6e2e5d8c7b4d04bb56d0c38c8e8f35f3c81
